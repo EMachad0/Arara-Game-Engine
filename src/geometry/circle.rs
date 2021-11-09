4ccf2bdca9ead1bff96aec21e60431774a7a38a7
igor <IgorFroehner>
2021-11-09 13:01:11 -0300

    feat: circle and cylinder

---
+++ src/geometry/circle.rs
@@ -0,0 +1,69 @@
+
+use std::f32::consts::PI;
+
+use super::vertex::Vertex;
+
+pub struct Circle {
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
+}
+
+impl Circle {
+    pub fn new(sector_count: u32) -> Self {
+        let circle = circle_points(sector_count);
+        let normal = [0.0, 1.0, 0.0];
+
+        let mut vertices: Vec<Vertex> = Vec::new();
+        
+        for (i, vertice) in circle.iter().enumerate() {
+            let position = [vertice.x, 0.0, vertice.y];
+            let tex_coords = [(vertice.x + 1.0) / 2.0, (vertice.y + 1.0) / 2.0];
+
+            vertices.push(Vertex {
+                position, 
+                normal,
+                tex_coords,
+            });
+        }
+
+        let mut indices: Vec<u32> = Vec::new();
+        let center_index = vertices.len() as u32;
+
+        for i in 0..center_index {
+            indices.push(center_index);
+            if i + 1 == center_index {
+                indices.push(0);
+            } else {
+                indices.push(i+1);
+            }
+            indices.push(i);
+        }
+
+        vertices.push(Vertex {
+            position: [0.0, 0.0, 0.0],
+            normal,
+            tex_coords: [0.5, 0.5],
+        });
+
+
+        Circle {
+            vertices,
+            indices,
+        }
+    }
+}
+
+pub fn circle_points(sector_count: u32) -> Vec<cgmath::Vector2::<f32>> {
+    let sector_step = 2.0 * PI / sector_count as f32;
+
+    let mut circle_vertices = Vec::new();
+    for i in 0..(sector_count + 1) {
+        let sector_angle = i as f32 * sector_step;
+        circle_vertices.push(cgmath::vec2 (
+            sector_angle.cos(), 
+            sector_angle.sin(), 
+        ));
+    }
+    circle_vertices
+}
+
--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -3,7 +3,11 @@
 pub mod vertex;
 pub mod cube;
 pub mod sphere;
+pub mod cylinder;
+pub mod circle;
 
 pub use vertex::Vertex;
 pub use cube::Cuboid;
 pub use sphere::Sphere;
+pub use cylinder::Cylinder;
+pub use circle::Circle;
