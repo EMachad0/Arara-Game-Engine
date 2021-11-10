4c966b8d1a6b8e2d02253bab0960c541ba4c65dd
EMachad0 <elitonmachadod200@gmail.com>
2021-11-10 16:33:04 -0300

    removed consts

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -3,12 +3,4 @@
 mod transform;
 mod transform_builder;
 
-pub mod consts {
-    use cgmath::*;
-    
-    pub const X_AXIS : Vector3::<f32> = Vector3::<f32>::new(1.0, 0.0, 0.0);
-    pub const Y_AXIS : Vector3::<f32> = Vector3::<f32>::new(0.0, 1.0, 0.0);
-    pub const Z_AXIS : Vector3::<f32> = Vector3::<f32>::new(0.0, 0.0, 1.0);
-}
-
 pub use transform_builder::TransformBuilder;
