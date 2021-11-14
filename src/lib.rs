4d9671937d6b6114a38739be0bf4e23da20b71f0
EMachad0 <elitonmachadod200@gmail.com>
2021-11-14 11:16:57 -0300

    refactor: remove arara_internal

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
-pub use arara_internal::*;
+pub mod prelude;
+
+pub mod camera {
+    pub use arara_camera::*;
+}
+
+pub mod geometry {
+    pub use arara_geometry::*;
+}
+
+pub mod shaders {
+    pub use arara_shaders::*;
+}
+
+pub mod transform {
+    pub use arara_transform::*;
+}
