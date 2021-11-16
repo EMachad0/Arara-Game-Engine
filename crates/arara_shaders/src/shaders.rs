c08c02934fe0fd09db0f6f717adb66ae0dd217be
EMachad0 <elitonmachadod200@gmail.com>
2021-11-16 16:52:26 -0300

    fix: file extension for glsang

--- crates/arara_shaders/src/shaders.rs
+++ crates/arara_shaders/src/shaders.rs
@@ -12,15 +12,15 @@ impl Default for Shaders {
 impl Shaders {
     pub fn from_color() -> Self {
         Self {
-            vertex_shader: include_str!("../../../assets/shaders/vertex_shader_src.glsl"),
-            fragment_shader: include_str!("../../../assets/shaders/fragment_shader_src.glsl"),
+            vertex_shader: include_str!("../../../assets/shaders/vertex_shader_src.vert"),
+            fragment_shader: include_str!("../../../assets/shaders/fragment_shader_src.frag"),
         }
     }
 
     pub fn from_texture() -> Self {
         Self {
-            vertex_shader: include_str!("../../../assets/shaders/vertex_shader_tex_src.glsl"),
-            fragment_shader: include_str!("../../../assets/shaders/fragment_shader_tex_src.glsl"),
+            vertex_shader: include_str!("../../../assets/shaders/vertex_shader_tex_src.vert"),
+            fragment_shader: include_str!("../../../assets/shaders/fragment_shader_tex_src.frag"),
         }
     }
 }
